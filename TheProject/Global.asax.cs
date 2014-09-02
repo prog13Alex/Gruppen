219a6c87b419b1d7fa50f5bced0956cbfbcf9182
Skvall <rickard.ekman@yh.nackademin.se>
2014-09-02 15:55:37 +0200

    Testar //hejhej

--- TheProject/Global.asax.cs
+++ TheProject/Global.asax.cs
@@ -17,5 +17,6 @@ namespace TheProject
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        //hejhej
     }
 }
