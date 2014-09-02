111147589889843aa14b2652488f88ba8e10f049
Skvall <skvall@gmail.com>
2014-09-02 17:19:28 +0200

    ta bort och lägg till class

---
+++ TheProject/Models/blabla.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheProject.Models
+{
+    public class blabla
+    {
+        public int AlexBallex { get; set; }
+
+    }
+}
